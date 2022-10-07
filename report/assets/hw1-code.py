8ad946b32278f53cf496b9d245f11a0f40d28fb6
Diogo Correia <me@diogotc.com>
2022-10-07 16:22:07 +0100

    Add Homework 1 report (#4)
    Co-authored-by: Diogo Correia <me@diogotc.com>
    Co-authored-by: Pesteves2002 <tomasesteves2002@gmail.com>
    Co-authored-by: Tomás Esteves <69385653+Pesteves2002@users.noreply.github.com>

---
+++ report/assets/hw1-code.py
@@ -0,0 +1,71 @@
+from operator import itemgetter
+import pandas as pd
+from scipy.io.arff import loadarff
+from sklearn import feature_selection, model_selection, tree, metrics, preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Reading the ARFF file
+data = loadarff("data/pd_speech.arff")
+df = pd.DataFrame(data[0])
+df["class"] = df["class"].str.decode("utf-8")
+
+# Separate features from the outcome (class)
+X = df.drop("class", axis=1)
+y = df["class"]
+
+# Split the dataset into a training set (70%) and a testing set (30%)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(
+    X, y, train_size=0.7, stratify=y, random_state=1
+)
+
+NUM_FEATURES = [5, 10, 40, 100, 250, 700]
+
+training_accurancy = []
+test_accurancy = []
+
+for num_features in NUM_FEATURES:
+    # Select the features with the greatest information gain (by mutual_info_classif)
+    kbest = feature_selection.SelectKBest(
+        score_func=lambda a, b: feature_selection.mutual_info_classif(
+            a, b, random_state=1
+        ),
+        k=num_features,
+    )
+    kbest.fit(X_train, y_train)
+
+    # Get the observation sets with only the selected N best features
+    X_train_cut = kbest.transform(X_train)
+    X_test_cut = kbest.transform(X_test)
+
+    # Fit the decision tree classifier
+    predictor = tree.DecisionTreeClassifier(random_state=1)
+    predictor.fit(X_train_cut, y_train)
+
+    # Use the decision tree to predict the outcome of the given observations
+    y_train_pred = predictor.predict(X_train_cut)
+    y_test_pred = predictor.predict(X_test_cut)
+
+    # Get the accuracy of each test
+    train_acc = metrics.accuracy_score(y_train, y_train_pred)
+    test_acc = metrics.accuracy_score(y_test, y_test_pred)
+
+    training_accurancy.append(train_acc)
+    test_accurancy.append(test_acc)
+
+plt.plot(
+    NUM_FEATURES,
+    training_accurancy,
+    label="Training Accuraccy",
+    marker="+",
+    color="#4caf50",
+)
+plt.plot(
+    NUM_FEATURES, test_accurancy, label="Test Accuraccy", marker=".", color="#ff5722"
+)
+
+plt.xlabel("Number of Selected Features")
+plt.ylabel("Accuracy")
+
+plt.legend()
+plt.show()
